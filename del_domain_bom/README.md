a44c1928ed9a35934580edca5a14affccdcb3fa6
AfroMonkey <moisalejandro@gmail.com>
2016-06-15 10:31:29 -0500

    [IMP] Remove domain from bom selection In the view "mrp.mrp_production_form_view", the field "bom_id" have no more domain

---
+++ del_domain_bom/README.md
@@ -0,0 +1,3 @@
+Delete domain BoM
+=================
+In the view "mrp.mrp_production_form_view", the field "bom_id" have no more domain
---
+++ del_domain_bom/__openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Delete domain BoM',
+    'version': '1.0',
+    'author': 'afromonkey',
+    'website': 'http://humanytek.com',
+    'depends': ['mrp'],
+    'data': [
+        'views/del_domain_bom.xml',
+    ],
+    'application': True,
+}
